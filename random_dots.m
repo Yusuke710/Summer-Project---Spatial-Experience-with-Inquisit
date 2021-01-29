1ddf6ae389c7de1d6772f2ca8736eea00a2c80f0
Yusuke710 <58585405+Yusuke710@users.noreply.github.com>
2021-01-30 00:43:09 +1100

    Add files via upload
    removed grey screen
    dot size changable
    plot square

--- random_dots.m
+++ random_dots.m
@@ -1,10 +1,10 @@
 clear all; close all; clc;
 
 % ask users how many images they want
-prompt = 'How many trials?';
-trials = input(prompt)
+prompt = 'How many images?';
+images = input(prompt)
 
-for i = 1:trials;
+for i = 1:images;
 
     % ask user the number of dots on screen
     prompt = 'How many dots? ';
@@ -12,18 +12,23 @@ for i = 1:trials;
 
     % make the figure full screen
     FigH = figure('Position', get(0, 'Screensize'), 'visible','off'); %remove 'visible','off'if you would like to display the figure
+    
     % generate dots
     x = rand(1, numPoints);
     y = rand(1, numPoints);
-    plot(x, y, 'k.');
+    sz =25; %MarkerSize
+    plot(x, y, 'k.','MarkerSize',sz);
+    pbaspect([1 1 1]) % control the ratio of the axis, it is square here
     
     % remove axis in plot
     set(gca,'XColor','none','YColor','none','TickDir','out') 
+    % remove grey background 
+    set(gcf,'color','w')
 
     % save images
         % name of the file
-        word1 = 'plot_test';
-        word2 = num2str(i);
+        word1 = num2str(numPoints);
+        word2 = 'RandomDots';
         word3 = '.jpg';
         str = strcat(word1, word2, word3);
         % specify the directory to save images
